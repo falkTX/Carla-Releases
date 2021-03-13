66a97b67ad93724f97ddab73ae3a74a015398a22
falkTX <falktx@falktx.com>
2021-03-13 11:54:18 +0000

    Cleanup, update carla

--- build.sh
+++ build.sh
@@ -35,7 +35,10 @@ BUILD_VERSION=$((${LAST_BUILD_VERSION} + 1))
 
 mkdir -p ${PAWPAW_BUILDDIR}
 echo ${BOOTSTRAP_VERSION} > ${PAWPAW_BUILDDIR}/.last-bootstrap-version
-echo ${BUILD_VERSION} > ${PAWPAW_BUILDDIR}/.last-build-version
+
+if [ ${BUILD_VERSION} -lt 4 ]; then
+    echo ${BUILD_VERSION} > ${PAWPAW_BUILDDIR}/.last-build-version
+fi
 
 echo "Carla-Releases build v${BUILD_VERSION}"
 
@@ -49,45 +52,14 @@ if [ ${BUILD_VERSION} -eq 1 ]; then
 fi
 
 if [ ${BUILD_VERSION} -eq 2 ]; then
-    # qt build takes too long on macos-universal target, download and use premade builds
-    if [ "${TARGET}" = "macos-universal" ]; then
-        CROSS_COMPILING=0
-        MACOS=1
-        MACOS_OLD=0
-        MACOS_UNIVERSAL=1
-        WIN32=0
-        source "${TRAVIS_BUILD_DIR}/PawPaw/setup/versions.sh"
-        mkdir -p "${PAWPAW_BUILDDIR}/qtbase-everywhere-src-${QT5_VERSION}"
-        touch "${PAWPAW_BUILDDIR}/qtbase-everywhere-src-${QT5_VERSION}/.stamp_configured"
-        touch "${PAWPAW_BUILDDIR}/qtbase-everywhere-src-${QT5_VERSION}/.stamp_built"
-        touch "${PAWPAW_BUILDDIR}/qtbase-everywhere-src-${QT5_VERSION}/.stamp_installed"
-        touch "${PAWPAW_BUILDDIR}/qtbase-everywhere-src-${QT5_VERSION}/.stamp_applied_01_force-10.12-universal-build.patch"
-        mkdir -p "${PAWPAW_BUILDDIR}/qtmacextras-everywhere-src-${QT5_VERSION}"
-        touch "${PAWPAW_BUILDDIR}/qtmacextras-everywhere-src-${QT5_VERSION}/.stamp_configured"
-        touch "${PAWPAW_BUILDDIR}/qtmacextras-everywhere-src-${QT5_VERSION}/.stamp_built"
-        touch "${PAWPAW_BUILDDIR}/qtmacextras-everywhere-src-${QT5_VERSION}/.stamp_installed"
-        mkdir -p "${PAWPAW_BUILDDIR}/qtsvg-everywhere-src-${QT5_VERSION}"
-        touch "${PAWPAW_BUILDDIR}/qtsvg-everywhere-src-${QT5_VERSION}/.stamp_configured"
-        touch "${PAWPAW_BUILDDIR}/qtsvg-everywhere-src-${QT5_VERSION}/.stamp_built"
-        touch "${PAWPAW_BUILDDIR}/qtsvg-everywhere-src-${QT5_VERSION}/.stamp_installed"
-        mkdir -p "${PAWPAW_BUILDDIR}/qttools-everywhere-src-${QT5_VERSION}"
-        touch "${PAWPAW_BUILDDIR}/qttools-everywhere-src-${QT5_VERSION}/.stamp_configured"
-        touch "${PAWPAW_BUILDDIR}/qttools-everywhere-src-${QT5_VERSION}/.stamp_built"
-        touch "${PAWPAW_BUILDDIR}/qttools-everywhere-src-${QT5_VERSION}/.stamp_installed"
-        pushd "${PAWPAW_DIR}/targets"
-        curl -L "https://falktx.com/data/pawpaw-qt-macos-universal.tar.xz" -o "pawpaw-qt-macos-universal.tar.xz" --fail
-        tar xvf pawpaw-qt-macos-universal.tar.xz
-        popd
-    fi
     ${TRAVIS_BUILD_DIR}/PawPaw/bootstrap-qt.sh ${TARGET}
     ${TRAVIS_BUILD_DIR}/PawPaw/.cleanup.sh ${TARGET}
     exit 0
 fi
 
-${TRAVIS_BUILD_DIR}/PawPaw/bootstrap-carla.sh ${TARGET}
-${TRAVIS_BUILD_DIR}/PawPaw/.cleanup.sh ${TARGET}
-
 if [ ${BUILD_VERSION} -eq 3 ]; then
+    ${TRAVIS_BUILD_DIR}/PawPaw/bootstrap-carla.sh ${TARGET}
+    ${TRAVIS_BUILD_DIR}/PawPaw/.cleanup.sh ${TARGET}
     exit 0
 fi
 
