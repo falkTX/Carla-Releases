60f522685c440b27f72e896c8bed6618f214ce67
falkTX <falktx@falktx.com>
2021-07-18 17:41:02 +0100

    Setup github actions

--- build.sh
+++ build.sh
@@ -14,69 +14,6 @@ if [ -z "${target}" ]; then
 fi
 
 # ---------------------------------------------------------------------------------------------------------------------
-# non-travis builds
-
-if [ -z "${BOOTSTRAP_VERSION}" ]; then
-    BOOTSTRAP_VERSION=1
-fi
-if [ -z "${TRAVIS_BUILD_DIR}" ]; then
-    TRAVIS_BUILD_DIR="$(pwd)"
-fi
-
-# ---------------------------------------------------------------------------------------------------------------------
-# check build step
-
-PAWPAW_DIR="${HOME}/PawPawBuilds"
-PAWPAW_BUILDDIR="${PAWPAW_DIR}/builds/${target}"
-
-if [ -e ${PAWPAW_BUILDDIR}/.last-bootstrap-version ]; then
-    LAST_BOOTSTRAP_VERSION=$(cat ${PAWPAW_BUILDDIR}/.last-bootstrap-version)
-else
-    LAST_BOOTSTRAP_VERSION=0
-fi
-
-if [ ${LAST_BOOTSTRAP_VERSION} -eq ${BOOTSTRAP_VERSION} ] && [ -e ${PAWPAW_BUILDDIR}/.last-build-version ]; then
-    LAST_BUILD_VERSION=$(cat ${PAWPAW_BUILDDIR}/.last-build-version)
-else
-    LAST_BUILD_VERSION=0
-fi
-
-BUILD_VERSION=$((${LAST_BUILD_VERSION} + 1))
-
-mkdir -p ${PAWPAW_BUILDDIR}
-echo ${BOOTSTRAP_VERSION} > ${PAWPAW_BUILDDIR}/.last-bootstrap-version
-
-if [ ${BUILD_VERSION} -le 4 ]; then
-    echo ${BUILD_VERSION} > ${PAWPAW_BUILDDIR}/.last-build-version
-fi
-
-echo "Carla-Releases build v${BUILD_VERSION}"
-
-# ---------------------------------------------------------------------------------------------------------------------
-# build dependencies according to version/step, caching files along the way
-
-if [ ${BUILD_VERSION} -eq 1 ]; then
-    ${TRAVIS_BUILD_DIR}/PawPaw/bootstrap-plugins.sh ${target}
-    ${TRAVIS_BUILD_DIR}/PawPaw/.cleanup.sh ${target}
-    exit 0
-fi
-
-if [ ${BUILD_VERSION} -eq 2 ]; then
-    ${TRAVIS_BUILD_DIR}/PawPaw/bootstrap-qt.sh ${target}
-    ${TRAVIS_BUILD_DIR}/PawPaw/.cleanup.sh ${target}
-    exit 0
-fi
-
-if [ ${BUILD_VERSION} -eq 3 ]; then
-    ${TRAVIS_BUILD_DIR}/PawPaw/bootstrap-carla.sh ${target}
-    ${TRAVIS_BUILD_DIR}/PawPaw/.cleanup.sh ${target}
-    exit 0
-fi
-
-${TRAVIS_BUILD_DIR}/PawPaw/bootstrap-carla.sh ${target}
-${TRAVIS_BUILD_DIR}/PawPaw/.cleanup.sh ${target}
-
-# ---------------------------------------------------------------------------------------------------------------------
 # import PawPaw environment
 
 pushd PawPaw
